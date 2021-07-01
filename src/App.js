afe623cf2c14dae89cdb661168e0766245fa2413
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-06-30 21:20:40 -0400

    added in info from nasa api and website is working, need to add styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios"
+import Picture from "./Picture"
 
 
 
@@ -8,21 +9,30 @@ import axios from "axios"
 function App() {
 
   const [baseURL] = useState("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
-  const [picture, setPicture] = useState("This is a picture of space")
+  const [picture, setPicture] = useState([])
 
   useEffect(() => {
     axios.get(baseURL)
       .then((res) => {
         console.log(res.data)
+        setPicture(res.data)
       })
       .catch(error => { return error })
   }, [])
+
+
   return (
     <div className="App">
+      <header>
+        <h1>NASA Photo of the Day</h1>
+      </header>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+
       </p>
+      <main>
+        <Picture picture={picture} />
+      </main>
+
     </div>
   );
 }
--- src/Picture.js
+++ src/Picture.js
@@ -1,3 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios"
+
+
+
+const Picture = (props) => {
+    return (
+
+        < section >
+            {
+                console.log(props.picture)
+            }
+            <h3>Title: {props.picture.title}</h3>
+            <img src={props.picture.hdurl} />
+            <p>{props.picture.explanation}</p>
+
+        </section >
+    )
+}
+
+export default Picture
