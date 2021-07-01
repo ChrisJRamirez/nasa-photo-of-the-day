a81fd3f58aba90960fdc3b0ace1b730372cdc4af
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-06-30 20:13:33 -0400

    beginning to work on module 3 and 4 project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios"
 
 
 
 
 function App() {
+
+  const [baseURL] = useState("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
+  const [picture, setPicture] = useState("This is a picture of space")
+
+  useEffect(() => {
+    axios.get(baseURL)
+      .then((res) => {
+        console.log(res.data)
+      })
+      .catch(error => { return error })
+  }, [])
   return (
     <div className="App">
       <p>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import axios from "axios"
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
